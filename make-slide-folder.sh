5ee416b3ef5fc7d8f839c597931d5b1123705a39
Michael Kirkedal Thomsen <kirkedal@acm.org>
2019-01-14 14:11:49 +0100

    Script for creating a collected folder with slides

---
+++ make-slide-folder.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit immediately if any command below fails.
+set -e
+
+echo "Generating a slides directory.."
+mkdir -p slides
+rm -f slides/*
+
+cd material
+for f in *; do
+  cd $f
+  for s in *.pdf; do
+    if [[ $s != "*.pdf" ]]; then
+      echo $s
+      if [[ $s == "slides.pdf" ]]; then
+        ln $s ../../slides/${f}.pdf
+      else
+        ln $s ../../slides/${f}-${s}
+      fi
+    fi
+  done
+  cd ..
+done
+cd ..
